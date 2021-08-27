8f54150f4b25621447c65d7957b96d43d51135db
Ezenwa Okoro <okoroezenwa@bitbucket.org>
2021-08-27 12:26:24 +0100

    Initial Commit

---
+++ CityWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CityWeather
+//
+//  Created by Ezenwa Okoro on 27/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
